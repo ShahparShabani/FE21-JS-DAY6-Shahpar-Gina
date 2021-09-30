956b56888eeb0dc2a1e89a549e6424dbe31ac882
shahparshabani <shahpar.shabani@g-experts.net>
2021-09-30 14:52:04 +0200

    fix challenge issue to displaye relevant modal :)

--- Shahpar/js/challenge.js
+++ Shahpar/js/challenge.js
@@ -13,7 +13,7 @@ for (let i=0 ;  i < booksParse.length ; i++) {
         booksParse[i].image
     }" class="card-img-top" alt="small car photo">
     <div class="card-body ${readParse ? "read" : "not-read"}">
-      <h5 class="card-title"><a class="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <h5 class="card-title"><a class="" data-bs-toggle="modal" data-bs-target="#exampleModal${i}">
       ${booksParse[i].title}
     </a></h5>
        
@@ -22,7 +22,7 @@ for (let i=0 ;  i < booksParse.length ; i++) {
   </div>  
   
   <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered ">
       <div class="modal-content">
         <div class="modal-header">
